6216e66f99010172c39a030f612d473eb7fe1f35
filipesndev <filipe.eduardo.neiva@gmail.com>
2020-07-18 13:29:13 -0300

    sd

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../global/styleglobal.css'
 import '../node_modules/bulma/css/bulma.min.css'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps}){
     return <Component {...pageProps} />
